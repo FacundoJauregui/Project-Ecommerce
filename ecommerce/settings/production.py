4133699e9cdb7b5dba43c6c1a25a05b2cf73f7c2
Facundo <facu.jaurey@gmail.com>
2022-08-11 11:23:19 -0300

    aws s3 added

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -55,5 +55,4 @@ AWS_HEADERS = {
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE= 'storages.backends.s3boto3.S3StaticStorage'
 
-STATIC_URL= f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-MEDIA_URL= f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+STATIC_URL= f'https://{AWS_S3_CUSTOM_DOMAIN}/static/media/'
